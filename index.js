47f59b4baf5bc4f7daeeba423dd2ac2dc46cd846
INMD1 <87979171+INMD1@users.noreply.github.com>
2025-01-18 21:35:42 +0900

    초기 설정

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express(); 
+
+//Express 4.16.0버전 부터 body-parser의 일부 기능이 익스프레스에 내장 body-parser 연결 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    res.send("I Working ON");
+});
+
+
+app.listen(3000, () => console.log("Sever_Start"));
